0218e9af40a0c859c640f3fb18e2be3f20bd641d
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2013-02-16 20:42:57 -0300

    Initial Commit

---
+++ MosaicCollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MosaicCollectionView
+//
+//  Created by Ezequiel A Becerra on 2/16/13.
+//  Copyright (c) 2013 Betzerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MosaicCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MosaicCollectionView
+//
+//  Created by Ezequiel A Becerra on 2/16/13.
+//  Copyright (c) 2013 Betzerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
