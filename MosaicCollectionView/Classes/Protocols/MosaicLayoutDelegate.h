ae5599b03a63ebc92ce00c319cc18ea53b153f12
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2013-04-21 23:11:19 -0300

    MosaicLayoutDelegate protocol update

--- MosaicCollectionView/Classes/Protocols/MosaicLayoutDelegate.h
+++ MosaicCollectionView/Classes/Protocols/MosaicLayoutDelegate.h
@@ -12,9 +12,6 @@
 
 -(float)collectionView:(UICollectionView *)collectionView relativeHeightForItemAtIndexPath:(NSIndexPath*)indexPath doubleColumn:(BOOL)isDoubleColumn;
 -(BOOL)collectionView:(UICollectionView *)collectionView isDoubleColumnAtIndexPath:(NSIndexPath *)indexPath;
-
-@optional
-//  Actually this won't be optional but I don't want to refactor now
 -(NSUInteger)numberOfColumnsInCollectionView:(UICollectionView *)collectionView;
 
 @end
